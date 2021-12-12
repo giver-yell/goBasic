e8b61a5011fae8379cea06e5c7c5e39ceb62a73a
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-12 17:56:53 +0900

    8. 変数宣言

--- lesson.go
+++ lesson.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"os/user"
-	"time"
-)
+import "fmt"
 
 // func init() {
 // 	fmt.Println("init")
@@ -14,8 +10,29 @@ import (
 // fmt.Println("bazz")
 // }
 
+var (
+	i    int     = 1
+	f64  float64 = 1.2
+	s    string  = "test"
+	t, f bool    = true, false
+)
+
+func foo() {
+	xi := 1
+	xi = 2
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
+	fmt.Printf("%T\n", xf64)
+	fmt.Printf("%T\n", xi)
+}
+
 func main() {
+	fmt.Println(i, f64, s, t, f)
+	foo()
+
 	// bazz()
-	fmt.Println("hello world", time.Now())
-	fmt.Println(user.Current())
+	// fmt.Println("hello world", time.Now())
+	// fmt.Println(user.Current())
 }
