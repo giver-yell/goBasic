75a28e934cf50a9ec008474e40ad1e9ba1247a8d
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-13 09:26:01 +0900

    10. 数値型 #3

--- lesson.go
+++ lesson.go
@@ -1,22 +1,56 @@
 package main
 
-import "fmt"
+// 10.数値型
+// func main() {
+// var (
+// 	u8  uint8     = 255
+// 	i8  int8      = 127
+// 	f32 float32   = 0.2
+// 	c64 complex64 = -5 + 12i
+// )
 
-const Pi = 3.14
+// fmt.Println(u8, i8, f32, c64)
 
-const (
-	Username = "test_user"
-	Password = "password"
-)
+// fmt.Printf("type=%T, value=%v", u8, u8)
+
+// fmt.Println("1 + 1 =", 1+1)
+// fmt.Println("10 - 1 =", 10-1)
+// fmt.Println("10 / 2 =", 10/2)
+// fmt.Println("10 / 3 =", 10/3)
+// fmt.Println("10.0 / 3 =", 10.0/3)
+// fmt.Println("10 / 3.0 =", 10/3.0)
+// fmt.Println("10 % 2 =", 10%2)
+// fmt.Println("10 % 3 =", 10%3)
+
+// x := 0
+// x++
+// fmt.Println(x)
+// x--
+// fmt.Println(x)
+
+// 	fmt.Println(1 << 0) // 0001
+// 	fmt.Println(1 << 1) // 0010
+// 	fmt.Println(1 << 2) // 0100
+// 	fmt.Println(1 << 3) // 1000
+
+// }
+
+// 定数
+// const Pi = 3.14
+
+// const (
+// 	Username = "test_user"
+// 	Password = "password"
+// )
 
 // var big int = 9223372036854775807 + 1 // オーバーフロー
-const Big = 9223372036854775807 + 1
+// const Big = 9223372036854775807 + 1
 
-func main() {
-	fmt.Println(Pi, Username, Password)
-	// fmt.Printf("%T", Pi)
-	fmt.Println(Big - 1)
-}
+// func main() {
+// 	fmt.Println(Pi, Username, Password)
+// 	// fmt.Printf("%T", Pi)
+// 	fmt.Println(Big - 1)
+// }
 
 // func init() {
 // 	fmt.Println("init")
