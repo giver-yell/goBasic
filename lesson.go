489a5f7f7da8f6aa726c63b5ab6ac3f04f311ace
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-19 15:30:55 +0900

    17. map #3

--- lesson.go
+++ lesson.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+// 17.map
+func main() {
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m)
+	fmt.Println(m["apple"])
+	m["banana"] = 300
+	fmt.Println(m)
+	m["new"] = 500
+	fmt.Println(m)
+
+	fmt.Println(m["nothing"])
+
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
+
+	m2 := make(map[string]int)
+	m2["pc"] = 5000
+	fmt.Println(m2)
+
+	// nil メモリ上にないので
+	// var m3 map[string]int
+	// m3["pc"] = 5000
+	// fmt.Println(m3)
+
+	var s []int
+	if s == nil {
+		fmt.Println("nil")
+	}
+}
+
 // 16.スライスのmakeとcap
 // func main() {
 // n := make([]int, 3, 5)
