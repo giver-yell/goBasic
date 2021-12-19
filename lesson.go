fc5fbd06930572527582863b1a81b5966f31a075
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-19 15:42:35 +0900

    コメントアウト

--- lesson.go
+++ lesson.go
@@ -1,17 +1,15 @@
 package main
 
-import "fmt"
-
 // 18. バイト型
-func main() {
-	b := []byte{72, 73}
-	fmt.Println(b)
-	fmt.Println(string(b))
+// func main() {
+// 	b := []byte{72, 73}
+// 	fmt.Println(b)
+// 	fmt.Println(string(b))
 
-	c := []byte("HI")
-	fmt.Println(c)
-	fmt.Println(string(c))
-}
+// 	c := []byte("HI")
+// 	fmt.Println(c)
+// 	fmt.Println(string(c))
+// }
 
 // 17.map
 // func main() {
