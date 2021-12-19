776a72fb25e6eeeea74b8ace3a88f12d343fbcce
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-19 15:42:04 +0900

    18.バイト型 #3

--- lesson.go
+++ lesson.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+// 18. バイト型
+func main() {
+	b := []byte{72, 73}
+	fmt.Println(b)
+	fmt.Println(string(b))
+
+	c := []byte("HI")
+	fmt.Println(c)
+	fmt.Println(string(c))
+}
+
 // 17.map
 // func main() {
 // 	m := map[string]int{"apple": 100, "banana": 200}
