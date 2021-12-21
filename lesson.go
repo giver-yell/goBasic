4e6f5d08875a55517de9bed0b3dfd965df509c06
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-21 09:29:11 +0900

    セクション3 演習

--- lesson.go
+++ lesson.go
@@ -1,5 +1,25 @@
 package main
 
+// セクション3 演習
+/*
+演習
+Q1. 以下の1.11をint型に変換して出力してください。
+
+f := 1.11
+
+Q2. コードを書かずに以下の出力結果を答えてください。
+
+s := []int{1, 2, 5, 6, 2, 3, 1}
+fmt.Println(s[2:4])
+Q3. 以下のコードを実行した時に
+
+fmt.Printf("%T %v", m, m)
+
+以下のような出力結果となるmを作成してください。
+
+map[string]int map[Mike:20 Nancy:24 Messi:30]
+*/
+
 // 21.可変長変数
 // func foo(params ...int) {
 // 	fmt.Println(len(params), params)
