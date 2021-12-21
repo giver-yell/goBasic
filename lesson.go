2363e12fe9821ef03ff2cc2284f1ba0b4262dc25
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-21 09:28:21 +0900

    コメントアウト

--- lesson.go
+++ lesson.go
@@ -1,26 +1,24 @@
 package main
 
-import "fmt"
-
 // 21.可変長変数
-func foo(params ...int) {
-	fmt.Println(len(params), params)
-	for _, param := range params {
-		fmt.Println(param)
-	}
-}
-func main() {
-	foo()
-	foo(10)
-	foo(10, 20)
-	foo(10, 20, 30)
+// func foo(params ...int) {
+// 	fmt.Println(len(params), params)
+// 	for _, param := range params {
+// 		fmt.Println(param)
+// 	}
+// }
+// func main() {
+// 	foo()
+// 	foo(10)
+// 	foo(10, 20)
+// 	foo(10, 20, 30)
 
-	// スライス
-	s := []int{1, 2, 3}
-	fmt.Println(s)
+// 	// スライス
+// 	s := []int{1, 2, 3}
+// 	fmt.Println(s)
 
-	foo(s...)
-}
+// 	foo(s...)
+// }
 
 // 20.クロージャ
 // func incrementGenerator() func() int {
