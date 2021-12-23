42d7c89b4b75a0abc0e2052bd92943a24102101d
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-23 09:34:16 +0900

    25. for #5

--- section4.go
+++ section4.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
+// 25.for
+func main() {
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			fmt.Println("continue")
+			continue
+		}
+
+		if i > 5 {
+			fmt.Println("break")
+			break
+		}
+		fmt.Println(i)
+	}
+
+	sum := 1
+	for sum < 10 {
+		sum += sum
+		fmt.Println(sum)
+	}
+	fmt.Println(sum)
+}
+
 // 24.if
 // func by2(num int) string {
 // 	if num%2 == 0 {
