c1a91780a0b32c5b9177111ebba88903ab9518d7
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-23 09:44:48 +0900

    コメントアウト

--- section4.go
+++ section4.go
@@ -1,38 +1,36 @@
 package main
 
-import "fmt"
-
 // 26.range
-func main() {
-	l := []string{"python", "go", "ruby"}
-	for i := 0; i < len(l); i++ {
-		fmt.Println(i, l[i])
-	}
+// func main() {
+// 	l := []string{"python", "go", "ruby"}
+// 	for i := 0; i < len(l); i++ {
+// 		fmt.Println(i, l[i])
+// 	}
 
-	// 違う書き方
-	for i, v := range l {
-		fmt.Println(i, v)
-	}
+// 	// 違う書き方
+// 	for i, v := range l {
+// 		fmt.Println(i, v)
+// 	}
 
-	// iを使用しない
-	for _, v := range l {
-		fmt.Println(v)
-	}
+// 	// iを使用しない
+// 	for _, v := range l {
+// 		fmt.Println(v)
+// 	}
 
-	// map
-	m := map[string]int{"apple": 100, "banana": 200}
-	for k, v := range m {
-		fmt.Println(k, v)
-	}
+// 	// map
+// 	m := map[string]int{"apple": 100, "banana": 200}
+// 	for k, v := range m {
+// 		fmt.Println(k, v)
+// 	}
 
-	for k := range m {
-		fmt.Println(k)
-	}
+// 	for k := range m {
+// 		fmt.Println(k)
+// 	}
 
-	for _, v := range m {
-		fmt.Println(v)
-	}
-}
+// 	for _, v := range m {
+// 		fmt.Println(v)
+// 	}
+// }
 
 // 25.for
 // func main() {
