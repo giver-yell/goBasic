57471173dc7124b0525bfe389fbb9374bd8ac3d1
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-24 09:26:04 +0900

    28.defer #5

--- section4.go
+++ section4.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+// 28.defer
+func foo() {
+	defer fmt.Println("world foo")
+
+	fmt.Println("hello foo")
+}
+
+func main() {
+	// defer fmt.Println("world")
+
+	// foo()
+
+	// fmt.Println("hello")
+
+	// fmt.Println("run")
+	// defer fmt.Println("1")
+	// defer fmt.Println("2")
+	// defer fmt.Println("3")
+	// fmt.Println("success")
+
+	// deferの実用例
+	file, _ := os.Open("./section4.go")
+	defer file.Close()
+	data := make([]byte, 100)
+	file.Read(data)
+	fmt.Println(string(data))
+}
+
 // 27.switch
 // func getOsName() string {
 // 	return "deeefault"
