2dc52e96e73af519afbe1339d33f565adeda20d7
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-27 08:46:44 +0900

    37.演習 #7

--- section5.go
+++ section5.go
@@ -1,6 +1,41 @@
-package main
+// package main
 
 // セクション5 ポインタ
+// 演習
+// Q1. 以下の実行結果をコードを書かずに答えてください。エラーがおきますか？正しく表示されるとすると何が表示字されますか？
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var i int = 10
+// 	var p *int
+// 	p = &i
+// 	var j int
+// 	j = *p
+// 	fmt.Println(j)
+// }
+
+// Q2. 以下の実行結果をコードを書かずに答えてください。エラーがおきますか？正しく表示されるとすると何が表示されますか？
+
+// package main
+
+// func main() {
+// 	var i int = 100
+// 	var j int = 200
+// 	var p1 *int
+// 	var p2 *int
+// 	p1 = &i
+// 	p2 = &j
+// 	i = *p1 + *p2
+
+// 	p2 = p1
+// 	j = *p2 + i
+// 	fmt.Println(j)
+// }
 
 // 36.struct
 // type Vertex struct {
