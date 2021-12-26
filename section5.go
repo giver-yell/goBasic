f75ee8d1fd0d3afab4b26cee72840aab2db4c7e4
Ryohei Fujihara <giver.yell@gmail.com>
2021-12-27 08:36:54 +0900

    コメントアウト

--- section5.go
+++ section5.go
@@ -1,62 +1,61 @@
 package main
 
-import "fmt"
-
 // セクション5 ポインタ
+
 // 36.struct
-type Vertex struct {
-	// 小文字だとスコープがprivateになる
-	// X int
-	// Y int
-	X, Y int
-	S    string
-}
+// type Vertex struct {
+// 	// 小文字だとスコープがprivateになる
+// 	// X int
+// 	// Y int
+// 	X, Y int
+// 	S    string
+// }
 
-func changeVertex(v Vertex) {
-	v.X = 1000
-}
+// func changeVertex(v Vertex) {
+// 	v.X = 1000
+// }
 
-func changeVertex2(v *Vertex) {
-	v.X = 1000
-	// structでは同じ意味
-	// (*v).X = 1000
-}
+// func changeVertex2(v *Vertex) {
+// 	v.X = 1000
+// 	// structでは同じ意味
+// 	// (*v).X = 1000
+// }
 
-func main() {
-	v := Vertex{1, 2, "test"}
-	changeVertex(v)
-	fmt.Println(v)
+// func main() {
+// 	v := Vertex{1, 2, "test"}
+// 	changeVertex(v)
+// 	fmt.Println(v)
 
-	v2 := &Vertex{1, 2, "test"}
-	changeVertex2(v2)
-	fmt.Println(v2)
-	fmt.Println(*v2)
+// 	v2 := &Vertex{1, 2, "test"}
+// 	changeVertex2(v2)
+// 	fmt.Println(v2)
+// 	fmt.Println(*v2)
 
-	// v := Vertex{X: 1, Y: 2}
-	// fmt.Println(v)
-	// fmt.Println(v.X, v.Y)
-	// v.X = 100
-	// fmt.Println(v.X, v.Y)
+// 	// v := Vertex{X: 1, Y: 2}
+// 	// fmt.Println(v)
+// 	// fmt.Println(v.X, v.Y)
+// 	// v.X = 100
+// 	// fmt.Println(v.X, v.Y)
 
-	// v2 := Vertex{X: 1}
-	// fmt.Println(v2)
+// 	// v2 := Vertex{X: 1}
+// 	// fmt.Println(v2)
 
-	// v3 := Vertex{1, 2, "test"}
-	// fmt.Println(v3)
+// 	// v3 := Vertex{1, 2, "test"}
+// 	// fmt.Println(v3)
 
-	// v4 := Vertex{}
-	// fmt.Println(v4)
+// 	// v4 := Vertex{}
+// 	// fmt.Println(v4)
 
-	// var v5 Vertex
-	// fmt.Println(v5)
+// 	// var v5 Vertex
+// 	// fmt.Println(v5)
 
-	// // ポインタが返る
-	// v6 := new(Vertex)
-	// fmt.Println(v6)
+// 	// // ポインタが返る
+// 	// v6 := new(Vertex)
+// 	// fmt.Println(v6)
 
-	// v7 := &Vertex{}
-	// fmt.Println(v7)
-}
+// 	// v7 := &Vertex{}
+// 	// fmt.Println(v7)
+// }
 
 // 35.newとmakeの違い
 // func main() {
