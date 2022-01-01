fced86d8b7f104c9a02ae93e9afa2a42189af815
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-01 09:24:12 +0900

    コメントアウト

--- section6.go
+++ section6.go
@@ -2,38 +2,36 @@
 
 package main
 
-import "fmt"
-
 // 41.Embedded
-type Vertex struct {
-	x, y int
-}
+// type Vertex struct {
+// 	x, y int
+// }
 
-type Vertex3D struct {
-	Vertex
-	z int
-}
+// type Vertex3D struct {
+// 	Vertex
+// 	z int
+// }
 
-func (v Vertex3D) Area3D() int {
-	return v.x * v.y * v.z
-}
+// func (v Vertex3D) Area3D() int {
+// 	return v.x * v.y * v.z
+// }
 
-func (v *Vertex3D) Scale(i int) {
-	v.x = v.x * i
-	v.y = v.y * i
-	v.z = v.z * i
-}
+// func (v *Vertex3D) Scale(i int) {
+// 	v.x = v.x * i
+// 	v.y = v.y * i
+// 	v.z = v.z * i
+// }
 
-func New(x, y, z int) *Vertex3D {
-	return &Vertex3D{Vertex{x, y}, z}
-}
+// func New(x, y, z int) *Vertex3D {
+// 	return &Vertex3D{Vertex{x, y}, z}
+// }
 
-func main() {
-	v := New(3, 4, 5)
-	v.Scale(10)
-	fmt.Println(v.Area3D())
+// func main() {
+// 	v := New(3, 4, 5)
+// 	v.Scale(10)
+// 	fmt.Println(v.Area3D())
 
-}
+// }
 
 // 40. コンストラクタ
 /* pythonのclass例
