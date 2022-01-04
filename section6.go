7afc2ec760bea0a0ed9cabdd23009de535d754ee
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-04 09:48:46 +0900

    43.インターフェイスとダックタイピング #9

--- section6.go
+++ section6.go
@@ -2,6 +2,41 @@
 
 package main
 
+import "fmt"
+
+// 43.インターフェイスとダックタイピング
+type Human interface {
+	Say() string
+}
+
+type Person struct {
+	Name string
+}
+
+func (p *Person) Say() string {
+	p.Name = "Mr." + p.Name
+	fmt.Println(p.Name)
+	return p.Name
+}
+
+func DriveCar(human Human) {
+	if human.Say() == "Mr.Mike" {
+		fmt.Println("run")
+	} else {
+		fmt.Println("Get out")
+	}
+}
+
+func main() {
+	var mike Human = &Person{"Mike"}
+	var x Human = &Person{"X"}
+
+	// mike.Say()
+	DriveCar(mike)
+	DriveCar(x)
+
+}
+
 // 41.Embedded
 // type Vertex struct {
 // 	x, y int
