de6d50f44bc67a4bfa8de2e7c8dba4ab29491290
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-06 09:39:46 +0900

    セクション6演習 #9

--- section6.go
+++ section6.go
@@ -2,6 +2,74 @@
 
 package main
 
+import (
+	"fmt"
+)
+
+/*
+演習
+Q1. 以下に、7と表示されるメソッドを作成してください。
+
+package main
+
+import (
+    "fmt"
+)
+
+type Vertex struct{
+    X, Y int
+}
+
+func main(){
+    v := Vertex{3, 4}
+    fmt.Println(v.Plus())
+}
+Q2 X is 3! Y is 4! と表示されるStringerを作成してください。
+
+package main
+
+import (
+    "fmt"
+)
+
+type Vertex struct{
+    X, Y int
+}
+
+func main(){
+    v := Vertex{3, 4}
+    fmt.Println(v)
+}
+*/
+
+// Q1.
+// type Vertex struct {
+// 	X, Y int
+// }
+
+// func (v Vertex) Plus() int {
+// 	return v.X + v.Y
+// }
+
+// func main() {
+// 	v := Vertex{3, 4}
+// 	fmt.Println(v.Plus())
+// }
+
+// Q2.
+type Vertex struct {
+	X, Y int
+}
+
+func (v Vertex) String() string {
+	return fmt.Sprintf("X is %d! Y is %d!", v.X, v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v)
+}
+
 // 46.カスタムエラー
 // type UserNotFound struct {
 // 	UserName string
