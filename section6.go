b09ce4638536e88c30c90550eafa9315330daa8a
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-06 09:14:06 +0900

    コメントアウト

--- section6.go
+++ section6.go
@@ -2,30 +2,28 @@
 
 package main
 
-import "fmt"
-
 // 46.カスタムエラー
-type UserNotFound struct {
-	UserName string
-}
+// type UserNotFound struct {
+// 	UserName string
+// }
 
-func (e *UserNotFound) Error() string {
-	return fmt.Sprintf("User not found: %v", e.UserName)
-}
+// func (e *UserNotFound) Error() string {
+// 	return fmt.Sprintf("User not found: %v", e.UserName)
+// }
 
-func myFunc() error {
-	ok := false
-	if ok {
-		return nil
-	}
-	return &UserNotFound{UserName: "Mike"}
-}
+// func myFunc() error {
+// 	ok := false
+// 	if ok {
+// 		return nil
+// 	}
+// 	return &UserNotFound{UserName: "Mike"}
+// }
 
-func main() {
-	if err := myFunc(); err != nil {
-		fmt.Println(err)
-	}
-}
+// func main() {
+// 	if err := myFunc(); err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
 
 // 45.Stringer
 // type Person struct {
