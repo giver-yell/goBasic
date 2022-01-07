02b77b259b82a4704706c4656c3c230f1fada9f9
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-07 09:22:12 +0900

    49.goroutine と sync.WaitGroup #9

---
+++ section7.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 49.goroutine と sync.WaitGroup
+// 並列処理
+func goroutine(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func nomal(s string) {
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	// 並列処理
+	// 先にnomal()の処理が終了すると、gooroutine()が処理されないのでsync.WaitGroupを利用
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go goroutine("hello", &wg)
+	nomal("world")
+	wg.Wait()
+}
