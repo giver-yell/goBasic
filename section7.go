61db671665a78a6219ef64f9afca179717cd1467
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-10 11:00:01 +0900

    51.Buffered Channels #9

--- section7.go
+++ section7.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+// 51.Buffered Channels
+func main() {
+	// 第二引数でバッファの数を指定
+	ch := make(chan int, 2)
+	ch <- 100
+	fmt.Println(len(ch))
+	ch <- 200
+	fmt.Println(len(ch))
+	// close で channelを閉じる。closeがないと、forループで3つ目も取りに行こうとしてエラーになる
+	close(ch)
+
+	for c := range ch {
+		fmt.Println(c)
+	}
+	// 値を取り出した後はlen が0に戻る
+	fmt.Println(len(ch))
+
+	// 3つ目の受信はエラー
+	// ch <- 300
+	// fmt.Println(len(ch))
+}
+
 // 50.channel
 // func goroutine1(s []int, c chan int) {
 // 	sum := 0
