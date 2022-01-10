dfdaf4945c4cf1262bca2d9c6a4040cacfcb2461
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-10 11:05:41 +0900

    コメントアウト

--- section7.go
+++ section7.go
@@ -1,25 +1,23 @@
 package main
 
-import "fmt"
-
 // 52.channelのrangeとclose
-func goroutine(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-		fmt.Println(sum)
-	}
-	close(c)
-}
+// func goroutine(s []int, c chan int) {
+// 	sum := 0
+// 	for _, v := range s {
+// 		sum += v
+// 		fmt.Println(sum)
+// 	}
+// 	close(c)
+// }
 
-func main() {
-	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int, len(s))
-	go goroutine(s, c)
-	for i := range c {
-		fmt.Println(i)
-	}
-}
+// func main() {
+// 	s := []int{1, 2, 3, 4, 5}
+// 	c := make(chan int, len(s))
+// 	go goroutine(s, c)
+// 	for i := range c {
+// 		fmt.Println(i)
+// 	}
+// }
 
 // 51.Buffered Channels
 // func main() {
