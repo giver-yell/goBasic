0c99f55e5829d24d1028e28a9e5fb5acd94281f2
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-10 10:34:37 +0900

    50.channel #9

--- section7.go
+++ section7.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
+// 50.channel
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func goroutine2(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int) // 15, 15
+	go goroutine1(s, c)
+	go goroutine2(s, c)
+	// 受信が終わるまで待機
+	x := <-c
+	fmt.Println(x)
+	y := <-c
+	fmt.Println(y)
+
+}
+
 // 49.goroutine と sync.WaitGroup
 // 並列処理
 // func goroutine(s string, wg *sync.WaitGroup) {
