45a783efcfe53ad68ec714f71dd53d39a142aba4
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-10 10:35:03 +0900

    コメントアウト

--- section7.go
+++ section7.go
@@ -1,36 +1,33 @@
 package main
 
-import "fmt"
-
 // 50.channel
-func goroutine1(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum
-}
+// func goroutine1(s []int, c chan int) {
+// 	sum := 0
+// 	for _, v := range s {
+// 		sum += v
+// 	}
+// 	c <- sum
+// }
 
-func goroutine2(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum
-}
+// func goroutine2(s []int, c chan int) {
+// 	sum := 0
+// 	for _, v := range s {
+// 		sum += v
+// 	}
+// 	c <- sum
+// }
 
-func main() {
-	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int) // 15, 15
-	go goroutine1(s, c)
-	go goroutine2(s, c)
-	// 受信が終わるまで待機
-	x := <-c
-	fmt.Println(x)
-	y := <-c
-	fmt.Println(y)
-
-}
+// func main() {
+// 	s := []int{1, 2, 3, 4, 5}
+// 	c := make(chan int) // 15, 15
+// 	go goroutine1(s, c)
+// 	go goroutine2(s, c)
+// 	// 受信が終わるまで待機
+// 	x := <-c
+// 	fmt.Println(x)
+// 	y := <-c
+// 	fmt.Println(y)
+// }
 
 // 49.goroutine と sync.WaitGroup
 // 並列処理
