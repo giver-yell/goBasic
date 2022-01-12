82b112c65a90f525d569c46b3417a9aa5893864a
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-12 09:06:20 +0900

    55.channelとselect #9

--- section7.go
+++ section7.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+// 55.channelとselect
+func goroutine1(ch chan string) {
+	for {
+		ch <- "packet from 1"
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func goroutine2(ch chan string) {
+	for {
+		ch <- "packet from 2"
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go goroutine1(c1)
+	go goroutine2(c2)
+
+	for {
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
+	}
+}
+
 // 54.fan-out fan-in
 // func producer(first chan int) {
 // 	defer close(first)
