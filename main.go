e800960ec44eb12c9eed20bb9d316d704bd647b6
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-22 18:04:38 +0900

    68.regex #13

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
 /*
 セクション9
 */
+
+// 68.regex
+func main() {
+	match, _ := regexp.MatchString("a[a-z]+e", "apple")
+	fmt.Println(match)
+
+	r := regexp.MustCompile("a[a-z]+e")
+	ms := r.MatchString("apple")
+	fmt.Println(ms)
+
+	r2 := regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+	fs := r2.FindString("/view/test")
+	fmt.Println(fs)
+	fss := r2.FindStringSubmatch("/view/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
+	fss = r2.FindStringSubmatch("/edit/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
+	fss = r2.FindStringSubmatch("/save/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
+
+}
 
 // 67.time
 // postgresのtime の型は RFC3339
