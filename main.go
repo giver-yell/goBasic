41e0a04955bb1c54c261aa8a7db1debc02383747
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-23 11:16:09 +0900

    72.ioutil #13

--- main.go
+++ main.go
@@ -3,6 +3,31 @@
 */
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+)
+
+// 72.ioutil
+func main() {
+	// content, err := ioutil.ReadFile("main.go")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// fmt.Println(string(content))
+
+	//  ファイル出力
+	// if err := ioutil.WriteFile("ioutil_temp.go", content, 0666); err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// byte出力
+	r := bytes.NewBuffer([]byte("abc"))
+	content, _ := ioutil.ReadAll(r)
+	fmt.Println(string(content))
+}
+
 // 71.context
 // import (
 // 	"context"
