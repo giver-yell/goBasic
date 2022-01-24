6e88a02f257ef3e70e270c042975d89936d8548c
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-24 09:18:28 +0900

    74. json.UnmarshalとMarshalとエンコード #15

--- main.go
+++ main.go
@@ -3,6 +3,66 @@
 */
 package main
 
+// 74. json.UnmarshalとMarshalとエンコード
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type T struct{}
+
+type Person struct {
+	// jsonで隠す
+	// Name string `json:"-"`
+	Name string `json:"name,omitempty"`
+	// jsonでstringへ変換
+	// Age       int      `json:"age,string"`
+	// 0の場合隠す
+	Age       int      `json:"age,omitempty"`
+	Nicknames []string `json:"nicknames"`
+	// structを隠す場合はポインタ
+	T *T `json:"T,omitempty"`
+}
+
+// unmarshal変換
+func (p *Person) UnmarshalJSON(b []byte) error {
+	type Person2 struct {
+		Name string
+	}
+	var p2 Person2
+	err := json.Unmarshal(b, &p2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	p.Name = p2.Name + "!"
+	return err
+}
+
+// JSONの値変換
+// func (p Person) MarshalJSON() ([]byte, error) {
+// 	v, err := json.Marshal(&struct {
+// 		Name string
+// 	}{
+// 		Name: "Mr." + p.Name,
+// 	})
+// 	return v, err
+// }
+
+func main() {
+	// b := []byte(`{"name":"Mike","age":"20","nicknames":["a","b","c"]}`)
+	// b := []byte(`{"name":"","age":0,"nicknames":["a","b","c"]}`)
+	b := []byte(`{"name":"Mike","age":0,"nicknames":["a","b","c"]}`)
+
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(p.Name, p.Age, p.Nicknames)
+
+	v, _ := json.Marshal(p)
+	fmt.Println(string(v))
+}
+
 // 73.http
 // import (
 // 	"fmt"
