85c7725daf3e3a0a658204003162c23501f3b53a
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-24 09:34:06 +0900

    75.hmacでAPI認証 #15

--- main.go
+++ main.go
@@ -3,6 +3,41 @@
 */
 package main
 
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+// 75.hmacでAPI認証
+var DB = map[string]string{
+	"User1Key": "User1Secret",
+	"User2Key": "User2Secret",
+}
+
+func Server(apiKey, sign string, data []byte) {
+	apiSecret := DB[apiKey]
+	h := hmac.New(sha256.New, []byte(apiSecret))
+	h.Write(data)
+	expectedHMAC := hex.EncodeToString(h.Sum(nil))
+	fmt.Println(sign == expectedHMAC)
+}
+
+func main() {
+	const apiKey = "User2Key"
+	const apiSecret = "User2Secret"
+
+	data := []byte("data")
+	h := hmac.New(sha256.New, []byte(apiSecret))
+	h.Write(data)
+	sign := hex.EncodeToString(h.Sum(nil))
+
+	fmt.Println(sign)
+
+	Server(apiKey, sign, data)
+}
+
 // 74. json.UnmarshalとMarshalとエンコード
 // import (
 // 	"encoding/json"
