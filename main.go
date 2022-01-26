eb454f1a797e101da8e5556e9db7925e7485d8c0
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-27 08:42:14 +0900

    コメントアウト

--- main.go
+++ main.go
@@ -4,51 +4,51 @@
 package main
 
 // 80.JSON-RPC 2.0 over WebSocketでBitcoinの価格をリアルタイムに取得する
-import (
-	"log"
-	"net/url"
+// import (
+// 	"log"
+// 	"net/url"
 
-	"github.com/gorilla/websocket"
-)
+// 	"github.com/gorilla/websocket"
+// )
 
-type JsonRPC2 struct {
-	Version string      `json:"jsonrpc"`
-	Method  string      `json:"method"`
-	Params  interface{} `json:"params"`
-	Result  interface{} `json:"result,omitempty"`
-	Id      *int        `json:"id,omitempty"`
-}
-type SubscribeParams struct {
-	Channel string `json:"channel"`
-}
+// type JsonRPC2 struct {
+// 	Version string      `json:"jsonrpc"`
+// 	Method  string      `json:"method"`
+// 	Params  interface{} `json:"params"`
+// 	Result  interface{} `json:"result,omitempty"`
+// 	Id      *int        `json:"id,omitempty"`
+// }
+// type SubscribeParams struct {
+// 	Channel string `json:"channel"`
+// }
 
-func main() {
-	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
-	log.Printf("connecting to %s", u.String())
+// func main() {
+// 	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
+// 	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("dial:", err)
-	}
-	defer c.Close()
+// 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+// 	if err != nil {
+// 		log.Fatal("dial:", err)
+// 	}
+// 	defer c.Close()
 
-	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{"lightning_ticker_BTC_JPY"}}); err != nil {
-		log.Fatal("subscribe:", err)
-		return
-	}
+// 	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{"lightning_ticker_BTC_JPY"}}); err != nil {
+// 		log.Fatal("subscribe:", err)
+// 		return
+// 	}
 
-	for {
-		message := new(JsonRPC2)
-		if err := c.ReadJSON(message); err != nil {
-			log.Println("read:", err)
-			return
-		}
+// 	for {
+// 		message := new(JsonRPC2)
+// 		if err := c.ReadJSON(message); err != nil {
+// 			log.Println("read:", err)
+// 			return
+// 		}
 
-		if message.Method == "channelMessage" {
-			log.Println(message.Params)
-		}
-	}
-}
+// 		if message.Method == "channelMessage" {
+// 			log.Println(message.Params)
+// 		}
+// 	}
+// }
 
 // 77.iniでconfigファイルの設定を読み込む
 // import (
