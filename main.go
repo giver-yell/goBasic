8dec299d9ed371ffb7d419410bb005abe391dc10
Ryohei Fujihara <giver.yell@gmail.com>
2022-02-02 08:46:44 +0900

    88.Web Applications 4 - http.Redirect #19

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 /*
-セクション13
+セクション13 Web Applicationを作成
 */
 package main
 
+// 88.Web Applications 4 - http.Redirect
 // 87.Web Applications 3 - templateとhttp.ResponseWriterとhttp.Request
 // 86.Web Applications 2 - http.ListenAndServer
 // 85.Web Applications 1 -ioutil
@@ -42,8 +43,11 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// /view/test
 	// /view/ 以降のURLを取得
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
@@ -56,11 +60,25 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		// http.StatusInternalServerError: 500を返却
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
 	// 86.
 	// 87.
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 
 	// :の前に何も書かなければlocalhostを指定
 	// nilでデフォルトのhandler → / で404を返却
