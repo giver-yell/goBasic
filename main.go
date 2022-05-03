62efc600f83810d51e1d97d5ca31d7b0e99c6a17
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-03 10:55:28 +0900

    new #21

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func init() {
-	fmt.Println("Init!")
-}
-
-func bazz() {
-	fmt.Println("Bazz")
-}
-
+/* 5.ポインタ */
+// 35. newとmakeの違い
 func main() {
-	bazz()
-	fmt.Println("Hello, world")
+	// メモリ領域確保はnew
+	var p *int = new(int)
+	fmt.Println(*p) // 0
+	*p++
+	fmt.Println(*p) // 1
+
+	// メモリ領域を確保しない
+	var p2 *int
+	fmt.Println(p2) //
 }
+
+// 34. ポインタ
+// func one(x *int) {
+// 	*x = 1
+// }
+
+// func main() {
+// 	var n int = 100
+// 	one(&n)
+// 	fmt.Println(n)
+// 	// fmt.Println(&n)
+
+// 	// var p *int = &n
+// 	// fmt.Println(p)
+// 	// fmt.Println(*p)
+// }
+
+//3. 定義
+// func init() {
+// 	fmt.Println("Init!")
+// }
+
+// func bazz() {
+// 	fmt.Println("Bazz")
+// }
+
+// func main() {
+// 	bazz()
+// 	fmt.Println("Hello, world")
+// }
