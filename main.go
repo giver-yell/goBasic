0dfc5259824d2b1d23ff6a5f0291452257d1a7aa
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-04 08:11:38 +0900

    newとmake追記 #21

--- main.go
+++ main.go
@@ -5,31 +5,34 @@ import "fmt"
 /* 5.ポインタ */
 // 35. newとmakeの違い
 func main() {
+	// make: ポインタを返さない
 	// slice
 	s := make([]int, 0)
-	fmt.Printf("%T\n", s)
+	fmt.Printf("%T\n", s) // []int
 
 	// map
 	m := make(map[string]int)
-	fmt.Printf("%T\n", m)
+	fmt.Printf("%T\n", m) // map[string]int
 
 	// channel
 	ch := make(chan int)
-	fmt.Printf("%T\n", ch)
+	fmt.Printf("%T\n", ch) // chan int
 
+	// new: ポインタを返す
 	// struct
 	var st = new(struct{})
-	fmt.Printf("%T\n", st)
+	fmt.Printf("%T\n", st) // *struct {}
 
 	// メモリ領域確保はnew
 	var p *int = new(int)
+	fmt.Println(p)  // 0x1400012c020
 	fmt.Println(*p) // 0
 	*p++
 	fmt.Println(*p) // 1
 
 	// メモリ領域を確保しない
 	var p2 *int
-	fmt.Println(p2) //
+	fmt.Println(p2) // nil
 }
 
 // 34. ポインタ
