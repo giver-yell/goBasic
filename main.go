91adca6dfce0ac4be2e4b92fa081f8749129d689
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-07 10:25:31 +0900

    13.型変換 #21

--- main.go
+++ main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
-	"os/user"
-	"time"
+	"strconv"
 )
 
 /* 3.定義 */
+// 13.型変換
+func main() {
+	// intからfloat
+	var x int = 1
+	xx := float64(x)
+	fmt.Printf("%T %v %f\n", xx, xx, xx)
+
+	// stringからintへの型変換はstrconv.Atoi(A:asciiアスキー)
+	var s string = "14"
+	i, _ := strconv.Atoi(s)
+	fmt.Printf("%T %v\n", i, i)
+
+	// ASCII
+	h := "Hello world"
+	fmt.Println(string(h[0])) // H
+}
 
 // 7.import
-func main() {
-	fmt.Println("Hello world", time.Now())
-	fmt.Println(user.Current())
-}
+// func main() {
+// 	fmt.Println("Hello world", time.Now())
+// 	fmt.Println(user.Current())
+// }
 
 // 6.Hello world
 // func init() {
