4e37d1bfbd1a3372a32de4c59b20b6e61ef9675f
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-08 08:37:59 +0900

    15.slice, 14.配列 #21

--- main.go
+++ main.go
@@ -4,21 +4,46 @@ import "fmt"
 
 /* 3.定義 */
 
-// 14.配列
+// 15.slice
 func main() {
-	var a [2]int
-	a[0] = 100
-	a[1] = 200
-	fmt.Println(a)
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
+	fmt.Println(n[2])
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[:])
 
-	var b [2]int = [2]int{100, 200}
-	// 配列はサイズを変更できないのでエラー
-	// b = append(b, 300) // エラー
-	fmt.Println(b)
+	n[2] = 100
+	fmt.Println(n)
 
-	var c []int = []int{100, 200}
-	fmt.Println(c)
+	n = append(n, 100, 200, 300)
+	fmt.Println(n)
+
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+		[]int{6, 7, 8},
+	}
+	fmt.Println(board)
 }
+
+// 14.配列
+// func main() {
+// 	var a [2]int
+// 	a[0] = 100
+// 	a[1] = 200
+// 	fmt.Println(a)
+
+// 	var b [2]int = [2]int{100, 200}
+// 	// 配列はサイズを変更できないのでエラー
+// 	// b = append(b, 300) // エラー
+// 	fmt.Println(b)
+
+// 	// sliceはサイズ変更可能
+// 	var c []int = []int{100, 200}
+// 	fmt.Println(c)
+// }
 
 // 13.型変換
 // func main() {
