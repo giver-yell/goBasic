2f10808c42ee045000d548bce3b093a5b5906366
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-08 11:28:15 +0900

    func #21

--- main.go
+++ main.go
@@ -4,16 +4,44 @@ import "fmt"
 
 /* 3.定義 */
 
-// 18.byte
-func main() {
-	b := []byte{72, 73}
-	fmt.Println(b)
-	fmt.Println(string(b)) // HI
-
-	c := []byte("HI")
-	fmt.Println(c)
-	fmt.Println(string(c))
+// 19.func
+func add(x, y int) (int, int) {
+	return x + y, x - y
 }
+
+func calc(price, item int) (result int) {
+	result = price * item
+	return
+}
+
+func main() {
+	r1, r2 := add(10, 20)
+	fmt.Println(r1, r2)
+
+	r3 := calc(100, 2)
+	fmt.Println(r3)
+
+	// inner func
+	f := func(x int) {
+		fmt.Println("inner func", x)
+	}
+	f(1)
+
+	func(x int) {
+		fmt.Println("inner func", x)
+	}(1)
+}
+
+// 18.byte
+// func main() {
+// 	b := []byte{72, 73}
+// 	fmt.Println(b)
+// 	fmt.Println(string(b)) // HI
+
+// 	c := []byte("HI")
+// 	fmt.Println(c)
+// 	fmt.Println(string(c))
+// }
 
 // 17.map
 // func main() {
