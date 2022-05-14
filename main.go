b2385a31dda1f85c0bec24fc4c816debc7a2bf3e
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-14 12:10:31 +0900

    25.for #23

--- main.go
+++ main.go
@@ -1,24 +1,52 @@
 package main
 
-import "fmt"
-
 /* ステートメント */
+
+// 25.for
+// func main() {
+// 	for i := 0; i < 10; i++ {
+// 		if i == 3 {
+// 			fmt.Println("continue")
+// 			continue
+// 		}
+
+// 		if i > 5 {
+// 			fmt.Println("break")
+// 			break
+// 		}
+// 		fmt.Println(i)
+// 	}
+
+// 	sum := 1
+// 	// for ; sum < 10 {
+// 	for sum < 10 {
+// 		sum += sum
+// 		fmt.Println(sum)
+// 	}
+// 	fmt.Println(sum)
+
+// 	// 無限ループ
+// 	for {
+// 		fmt.Println("hello")
+// 	}
+// }
+
 // 24.if
-func by2(num int) string {
-	if num%2 == 0 {
-		return "ok"
-	} else {
-		return "no"
-	}
-}
+// func by2(num int) string {
+// 	if num%2 == 0 {
+// 		return "ok"
+// 	} else {
+// 		return "no"
+// 	}
+// }
 
-func main() {
-	result := by2(10)
-	if result == "ok" {
-		fmt.Println("great")
-	}
+// func main() {
+// 	result := by2(10)
+// 	if result == "ok" {
+// 		fmt.Println("great")
+// 	}
 
-	if result2 := by2(10); result2 == "ok" {
-		fmt.Println("great 2")
-	}
-}
+// 	if result2 := by2(10); result2 == "ok" {
+// 		fmt.Println("great 2")
+// 	}
+// }
