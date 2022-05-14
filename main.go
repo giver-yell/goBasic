23ba9a0f38a0e8abdadcc22dd8a1caa386006e3d
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-14 16:22:46 +0900

    29.log #23

--- main.go
+++ main.go
@@ -2,39 +2,68 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
 /* ステートメント */
 
-// 28.defer
-func foo() {
-	defer fmt.Println("world foo")
-
-	fmt.Println("hello foo")
+// 29.log
+func LoggingSettings(logFile string) {
+	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	// log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	log.SetOutput(multiLogFile)
 }
 
 func main() {
-	/*
-		defer fmt.Println("world")
+	LoggingSettings("test.log")
 
-		foo()
+	_, err := os.Open("aaa")
+	if err != nil {
+		log.Fatalln("Exit", err)
+	}
 
-		fmt.Println("HELLO")
-	*/
-	/*
-		fmt.Println("run")
-		defer fmt.Println(1)
-		defer fmt.Println(2)
-		defer fmt.Println(3)
-		fmt.Println("succsess")
-	*/
-	file, _ := os.Open("./main.go")
-	defer file.Close()
-	data := make([]byte, 100)
-	file.Read(data)
-	fmt.Println(string(data))
+	log.Println("loggint")
+	log.Printf("%T %v", "test", "test")
+
+	// fatalはexitされる
+	log.Fatalf("%T %v", "test", "test")
+	log.Fatalln("error!")
+
+	fmt.Println("ok")
 }
+
+// 28.defer
+// func foo() {
+// 	defer fmt.Println("world foo")
+
+// 	fmt.Println("hello foo")
+// }
+
+// func main() {
+// 	/*
+// 		defer fmt.Println("world")
+
+// 		foo()
+
+// 		fmt.Println("HELLO")
+// 	*/
+// 	/*
+// 		fmt.Println("run")
+// 		defer fmt.Println(1)
+// 		defer fmt.Println(2)
+// 		defer fmt.Println(3)
+// 		fmt.Println("succsess")
+// 	*/
+// 	file, _ := os.Open("./main.go")
+// 	defer file.Close()
+// 	data := make([]byte, 100)
+// 	file.Read(data)
+// 	fmt.Println(string(data))
+// }
 
 // 27.switch
 // func getOsName() string {
