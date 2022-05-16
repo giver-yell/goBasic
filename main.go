916c2bdaab2a233d7faaa8611d2ccfe1d4228c49
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-16 09:27:23 +0900

    40.コンストラクタ #25

--- main.go
+++ main.go
@@ -2,30 +2,59 @@ package main
 
 import "fmt"
 
+/* セクション6: Structオリエンテッド */
+
+// 40.コンストラクタ
 type Vertex struct {
-	X, Y int
+	x, y int
 }
 
 // 値レシーバ
 func (v Vertex) Area() int {
-	return v.X * v.Y
+	return v.x * v.y
 }
 
 // ポインタレシーバ
 func (v *Vertex) Scale(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
+	v.x = v.x * i
+	v.y = v.y * i
 }
 
-func Area(v Vertex) int {
-	return v.X * v.Y
+func New(x, y int) *Vertex {
+	return &Vertex{x, y}
 }
 
 func main() {
-	v := Vertex{3, 4}
-	// fmt.Println(Area(v))
-	// struct orientedと同様なことがgolagでもできる
-	// v.を入力すると、補完でArea()が出てくるので便利
+	v := New(3, 4)
 	v.Scale(10)
 	fmt.Println(v.Area())
 }
+
+// 39.ポインタレシーバと値レシーバ
+// type Vertex struct {
+// 	X, Y int
+// }
+
+// // 値レシーバ
+// func (v Vertex) Area() int {
+// 	return v.X * v.Y
+// }
+
+// // ポインタレシーバ
+// func (v *Vertex) Scale(i int) {
+// 	v.X = v.X * i
+// 	v.Y = v.Y * i
+// }
+
+// func Area(v Vertex) int {
+// 	return v.X * v.Y
+// }
+
+// func main() {
+// 	v := Vertex{3, 4}
+// 	// fmt.Println(Area(v))
+// 	// object orientedと同様なことがgolagでもできる
+// 	// v.を入力すると、補完でArea()が出てくるので便利
+// 	v.Scale(10)
+// 	fmt.Println(v.Area())
+// }
