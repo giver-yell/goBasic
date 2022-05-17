e9298f62c4ec8b2afc353900d4bbe97d6189ea87
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-17 20:41:49 +0900

    41.Embedded #25

--- main.go
+++ main.go
@@ -4,31 +4,73 @@ import "fmt"
 
 /* セクション6: Structオリエンテッド */
 
-// 40.コンストラクタ
+// 41.Embedded
 type Vertex struct {
 	x, y int
 }
 
-// 値レシーバ
 func (v Vertex) Area() int {
 	return v.x * v.y
 }
 
-// ポインタレシーバ
 func (v *Vertex) Scale(i int) {
 	v.x = v.x * i
 	v.y = v.y * i
 }
 
-func New(x, y int) *Vertex {
-	return &Vertex{x, y}
+type Vertex3D struct {
+	Vertex
+	z int
+}
+
+func (v Vertex3D) Area3D() int {
+	return v.x * v.y * v.z
+}
+
+// ポインタレシーバ
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
 }
 
 func main() {
-	v := New(3, 4)
-	v.Scale(10)
+	v := New(3, 4, 5)
+	// v.Scale(10)
 	fmt.Println(v.Area())
+	v.Scale3D(10)
+	fmt.Println(v.Area3D())
 }
+
+// 40.コンストラクタ
+// type Vertex struct {
+// 	x, y int
+// }
+
+// // 値レシーバ
+// func (v Vertex) Area() int {
+// 	return v.x * v.y
+// }
+
+// // ポインタレシーバ
+// func (v *Vertex) Scale(i int) {
+// 	v.x = v.x * i
+// 	v.y = v.y * i
+// }
+
+// func New(x, y int) *Vertex {
+// 	return &Vertex{x, y}
+// }
+
+// func main() {
+// 	v := New(3, 4)
+// 	v.Scale(10)
+// 	fmt.Println(v.Area())
+// }
 
 // 39.ポインタレシーバと値レシーバ
 // type Vertex struct {
