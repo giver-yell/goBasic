8b40972a8e4ca99851bc769d9405bf6a62d49df6
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-24 08:49:33 +0900

    52.channelのrangeとclose #27

--- main.go
+++ main.go
@@ -2,20 +2,39 @@ package main
 
 import "fmt"
 
-// 51.Buffered Channels
-func main() {
-	ch := make(chan int, 2)
-	ch <- 100
-	fmt.Println(len(ch))
-	ch <- 200
-	fmt.Println(len(ch))
-	// ループする場合は、chの終わりを宣言しておく必要がある
-	close(ch)
+// 52.channelのrangeとclose
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
 
-	for c := range ch {
-		fmt.Println(c)
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int, len(s))
+	go goroutine1(s, c)
+	for i := range c {
+		fmt.Println(i)
 	}
 }
+
+// 51.Buffered Channels
+// func main() {
+// 	ch := make(chan int, 2)
+// 	ch <- 100
+// 	fmt.Println(len(ch))
+// 	ch <- 200
+// 	fmt.Println(len(ch))
+// 	// ループする場合は、chの終わりを宣言しておく必要がある
+// 	close(ch)
+
+// 	for c := range ch {
+// 		fmt.Println(c)
+// 	}
+// }
 
 // 50.channel
 // func goroutine1(s []int, c chan int) {
