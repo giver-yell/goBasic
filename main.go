988781101094605fffe30e4f0dc4a2fb6eed50c6
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-23 09:40:16 +0900

    50.51. channel #27

--- main.go
+++ main.go
@@ -1,30 +1,70 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
+
+// 51.Buffered Channels
+func main() {
+	ch := make(chan int, 2)
+	ch <- 100
+	fmt.Println(len(ch))
+	ch <- 200
+	fmt.Println(len(ch))
+	// ループする場合は、chの終わりを宣言しておく必要がある
+	close(ch)
+
+	for c := range ch {
+		fmt.Println(c)
+	}
+}
+
+// 50.channel
+// func goroutine1(s []int, c chan int) {
+// 	sum := 0
+// 	for _, v := range s {
+// 		sum += v
+// 	}
+// 	c <- sum
+// }
+
+// func goroutine2(s []int, c chan int) {
+// 	sum := 0
+// 	for _, v := range s {
+// 		sum += v
+// 	}
+// 	c <- sum
+// }
+
+// func main() {
+// 	s := []int{1, 2, 3, 4, 5}
+// 	c := make(chan int) // 15, 15
+// 	go goroutine1(s, c)
+// 	go goroutine2(s, c)
+// 	x := <-c
+// 	fmt.Println(x)
+// 	y := <-c
+// 	fmt.Println(y)
+// }
 
 // 49.goroutineとsync.WaitGroup（軽量のスレッド、並列処理）
-func goroutine(s string, wg *sync.WaitGroup) {
-	for i := 0; i < 5; i++ {
-		// time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
-	}
-	wg.Done()
-}
-func normal(s string) {
-	for i := 0; i < 5; i++ {
-		// time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
-	}
-}
+// func goroutine(s string, wg *sync.WaitGroup) {
+// 	for i := 0; i < 5; i++ {
+// 		// time.Sleep(100 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// 	wg.Done()
+// }
+// func normal(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		// time.Sleep(100 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
 
-func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go goroutine("world", &wg)
-	normal("hello")
+// func main() {
+// 	var wg sync.WaitGroup
+// 	wg.Add(1)
+// 	go goroutine("world", &wg)
+// 	normal("hello")
 
-	wg.Wait()
-}
+// 	wg.Wait()
+// }
