927686b4c92a78cac154099036124213b5e4f9b7
Ryohei Fujihara <giver.yell@gmail.com>
2022-05-29 17:27:50 +0900

    演習 #27

--- main.go
+++ main.go
@@ -1,44 +1,90 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-// 57.sync.Mutex
-type Counter struct {
-	v   map[string]int
-	mux sync.Mutex
+/*
+演習
+Q1. 以下のように文字列を連結させて出力したいコードがありますが、
+
+test1!
+test1!test2!
+test1!test2!test3!
+test1!test2!test3!test4!
+以下のコードには間違いがあります。上記の出力になるように修正してください。
+
+package main
+
+import "fmt"
+
+func goroutine(s []string, c chan int){
+    sum := ""
+    for _, v := range s{
+        sum += v
+        c <- sum
+    }
 }
 
-func (c *Counter) Inc(key string) {
-	c.mux.Lock()
-	defer c.mux.Unlock()
-	c.v[key]++
+func main(){
+    words := []string{"test1!", "test2!", "test3!", "test4!"}
+    c := make(chan int)
+    go goroutine(words, c)
+    for w := range c{
+        fmt.Println(w)
+    }
 }
+*/
 
-func (c *Counter) Value(key string) int {
-	c.mux.Lock()
-	defer c.mux.Unlock()
-	return c.v[key]
+func goroutine(s []string, c chan string) {
+	defer close(c)
+	sum := ""
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
 }
 
 func main() {
-	c := Counter{v: make(map[string]int)}
-	go func() {
-		for i := 0; i < 10; i++ {
-			c.Inc("Key")
-		}
-	}()
-	go func() {
-		for i := 0; i < 10; i++ {
-			c.Inc("Key")
-		}
-	}()
-	time.Sleep(1 * time.Second)
-	fmt.Println(c, c.Value("Key"))
+	words := []string{"test1!", "test2!", "test3!", "test4!"}
+	c := make(chan string)
+	go goroutine(words, c)
+	for w := range c {
+		fmt.Println(w)
+	}
 }
+
+// 57.sync.Mutex
+// type Counter struct {
+// 	v   map[string]int
+// 	mux sync.Mutex
+// }
+
+// func (c *Counter) Inc(key string) {
+// 	c.mux.Lock()
+// 	defer c.mux.Unlock()
+// 	c.v[key]++
+// }
+
+// func (c *Counter) Value(key string) int {
+// 	c.mux.Lock()
+// 	defer c.mux.Unlock()
+// 	return c.v[key]
+// }
+
+// func main() {
+// 	c := Counter{v: make(map[string]int)}
+// 	go func() {
+// 		for i := 0; i < 10; i++ {
+// 			c.Inc("Key")
+// 		}
+// 	}()
+// 	go func() {
+// 		for i := 0; i < 10; i++ {
+// 			c.Inc("Key")
+// 		}
+// 	}()
+// 	time.Sleep(1 * time.Second)
+// 	fmt.Println(c, c.Value("Key"))
+// }
 
 // 56.Default Selection と for break
 // func main() {
