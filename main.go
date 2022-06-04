498c31836861967ae29ff735a1bd3ff299c11191
Ryohei Fujihara <giver.yell@gmail.com>
2022-06-05 06:54:45 +0900

    71.context #31

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// 71.context
+func longProcess(ctx context.Context, ch chan string) {
+	fmt.Println("run")
+	time.Sleep(2 * time.Second)
+	fmt.Println("finish")
+	ch <- "result"
+}
+
+func main() {
+	ch := make(chan string)
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	go longProcess(ctx, ch)
+
+CTXLOOP:
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			break CTXLOOP
+		case <-ch:
+			fmt.Println("success")
+			break CTXLOOP
+		}
+	}
+	fmt.Println("#######")
+}
+
 // 70.iota
 // const (
 // 	c1 = iota
