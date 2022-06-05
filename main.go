68d545d67c14e948e2090b77acd99ec5179478fa
Ryohei Fujihara <giver.yell@gmail.com>
2022-06-05 17:24:42 +0900

    73.http #33

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// 73.http
+func main() {
+	// resp, _ := http.Get("http://example.com")
+	// defer resp.Body.Close()
+	// body, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
+
+	base, err := url.Parse("http://example.com/abdeojo")
+	reference, _ := url.Parse("/test?a=1&b=2")
+	endpoint := base.ResolveReference(reference).String()
+	fmt.Println(base, err)
+	fmt.Println(endpoint)
+	req, _ := http.NewRequest("GET", endpoint, nil)
+	// req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer([]byte("password")))
+	req.Header.Add("If-None-Match", `W/"wyzzy"`)
+	q := req.URL.Query()
+	q.Add("c", "3&%")
+	fmt.Println(q)
+	fmt.Println(q.Encode())
+	req.URL.RawQuery = q.Encode()
+
+	var client *http.Client = &http.Client{}
+	resp, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
