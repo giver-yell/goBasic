cf54f374598a0b3656953750970a8d96d2eb4cd4
Ryohei Fujihara <giver.yell@gmail.com>
2022-06-09 07:43:58 +0900

    76.Semaphore #35

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+)
+
+// 76.Semaphore
+// 同時にgoroutineが実行できる数を指定
+var s *semaphore.Weighted = semaphore.NewWeighted(1)
+
+func longProcess(ctx context.Context) {
+	// 処理待ちのキャンセル
+	isAcquire := s.TryAcquire(1)
+	if !isAcquire {
+		fmt.Println("Could not get lock")
+		return
+	}
+	// ロック
+	// if err := s.Acquire(ctx, 1); err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// リリース
+	defer s.Release(1)
+
+	fmt.Println("Wait...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Done")
+}
+
+func main() {
+	ctx := context.TODO()
+	go longProcess(ctx)
+	go longProcess(ctx)
+	go longProcess(ctx)
+	time.Sleep(2 * time.Second)
+	go longProcess(ctx)
+	time.Sleep(5 * time.Second)
+}
